657201678a8475cda765de90ba03071553b96e7e
jray6055 <62913470+jray6055@users.noreply.github.com>
2020-11-13 15:13:33 -0600

    Update pychecker.py

--- pychecker.py
+++ pychecker.py
@@ -2,14 +2,6 @@ import requests
 
 HOST = "http://34.121.122.205:5000"
 
-#md5 testing 
-md5_testingCode = {
-    "Pokemon": "4c60441fe7a879ce1dc5cb98ba327381"
-    "water": "9460370bb0ca1c98a779b1bcc6861c2c"
-    "highlighter": "9df70b23c8f3446b1aeca32b179649ff"
-}
-
-
 
 #----------------Here is the new script---------------------
 import requests
